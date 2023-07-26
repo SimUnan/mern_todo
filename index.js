02411e3c7299fbbd4c55ab2547fa662ff1d45401
SimUnan <118753881+SimUnan@users.noreply.github.com>
2023-07-26 16:40:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: "http://localhost:5173", credentials: true}));
 app.use(morgan('tiny'));
 app.use(cookieParser());  
 
